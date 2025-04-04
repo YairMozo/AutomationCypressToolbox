0dacd6a6c91cd90aa0d4bc46095be4d86a18b720
Yair Mozo <yairmozo@habi.co>
2025-04-03 22:25:30 -0500

    proyecto toolbox

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,40 @@
+import DashboardPage from '../support/pages/DashboardPage';
+import HomePage from '../support/pages/HomePage';
+import RegistrationFormPage from '../support/pages/RegistrationFormPage';
+import SignupPage from '../support/pages/SignupPage';
+import { generateUserData } from '../support/utils/testData';
+
+describe('Registration', () => {
+  it('should successfully register a new user', () => {
+    // Generate random user data for this test
+    const userData = generateUserData();
+    
+    // Step 1: Navigate to the website and click on register
+    HomePage.visit().clickRegister();
+
+    // Step 2: Enter name and email for signup
+    SignupPage.enterName(userData.username)
+              .enterEmail(userData.email)
+              .clickSignupButton();
+
+    // Step 3: Fill out the complete registration form
+    RegistrationFormPage.selectGender('male')
+                        .enterPassword(userData.password)
+                        .selectDateOfBirth('15','11' , '2003')
+                        .checkNewsletterAndOffers()
+                        .enterFirstName(userData.firstName)
+                        .enterLastName(userData.lastName)
+                        .enterCompany(userData.company)
+                        .enterAddress(userData.address)
+                        .selectCountry(userData.country)
+                        .enterState(userData.state)
+                        .enterCity(userData.city)
+                        .enterZipcode(userData.zipcode)
+                        .enterMobileNumber(userData.mobileNumber)
+                        .clickCreateAccount();
+
+    // Step 4: Assertions to verify successful registration
+    DashboardPage.verifyAccountCreated()
+                .clickContinue();
+  });
+});
